ffa857ec09cb2b59e6b6e6d298e378a02eea3cea
Brandon Pacheco <BrandonAPacheco@Gmail.com>
2023-11-05 19:49:01 -0500

    added functions to users.controller; Not final.

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -80,3 +80,81 @@ export const GetAllUsers = async (req, res) => {
     return res.sendStatus(500);
   }
 };
+
+
+//Added by me, change if you need/want to ==========================================================
+
+export const GetUserById = async(req, res) => {
+  const id = req.body; //double check this, change if necessary: req.params.id
+  try{
+    const user = await UserService.getUserById(id);
+    if(!user){
+      return res.sendStatus(204);
+    }
+    return res.status(200).json(user);
+  } catch(error){
+    console.error("Error fetching user by ID:", error);
+    return res.sendStatus(500);    
+  }
+};
+
+//this is could be incorrectly implemented?? you should do this one.
+export const EmailExists = async(req, res) => {
+  const emailInput = req.body; // double check
+  try{
+    const condition = await UserService.emailExists(emailInput);
+    if(!condition){
+      return condition; //returning the error?
+    }
+    return res.status(200).json({msg: "This email is not currently used."}); //returns the true statement
+  } catch(error){
+    console.error("Error checking if email exists in database:", error);
+    return res.sendStatus(500);
+  }
+};
+
+export const GetUsersByPage = async(req, res) => {
+  const pageNumber = req.body; //double check
+  try{
+    const users = await UserService.getUsersByPage(pageNumber);
+    if(!users){
+      return res.sendStatus(204);
+    }
+    return res.status(200).json(users);
+  } catch(error){
+    console.error("Error fetching users by page:", error);
+    return res.sendStatus(500);    
+  }
+};
+
+export const UpdateUserById = async(req, res) => {
+  const { id, updateData } = req.body; //double check
+  try{
+    const user = await UserService.updateUserById(id, updateData);
+    if(!user){
+      return res.sendStatus(204);
+    }
+    return res.status(200).json(user);
+  } catch(error){
+    console.error("Error updating user by id:", error);
+    return res.sendStatus(500);    
+  }
+};
+
+export const DeleteUserById = async(req, res) => {
+  const id = req.body; //double check
+  try{
+    const condition = await UserService.deleteUserById(id);
+    if(!condition){
+      return res.status(500).json({msg: "Failed to delete the user."});
+    }
+    return res.status(200).json({msg: "User deleted successfully."});
+  } catch(error){
+    console.error("Error deleting user by id:", error);
+    return res.sendStatus(500);    
+  }
+};
+
+//functions that need to implemented: refreshtokens, isAdmin, getUserByEmail.
+
+

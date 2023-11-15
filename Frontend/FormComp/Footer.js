b38c9dcf4b3b75bef1c5a6afa3ad37f6b8503e67
TroyO4 <146477056+TroyO4@users.noreply.github.com>
2023-11-15 13:13:01 -0500

    Update Footer.js

--- Frontend/FormComp/Footer.js
+++ Frontend/FormComp/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Footer.css'
 
 function Footer() {
   return (
     <footer className="footer">
       {/* Add footer content here */}
-      &copy; {new Date().getFullYear()} Your App Name
+      &copy; {new Date().getFullYear()} AC Public Works
     </footer>
   );
 }

46bad689962df25b2dc4e3fb3f5fd3731a8a45cd
TroyO4 <146477056+TroyO4@users.noreply.github.com>
2023-12-01 16:22:34 -0500

    Update Vehicle.jsx

--- client/src/components/Tables/Vehicle.jsx
+++ client/src/components/Tables/Vehicle.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './Overview.css';
 
 function GetAllVehicles() {
   let [vehicles, setVehicles] = useState([]);

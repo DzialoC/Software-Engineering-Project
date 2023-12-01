fb37223e0229d1046fde2b7f9f90c285b3733def
TroyO4 <146477056+TroyO4@users.noreply.github.com>
2023-12-01 16:21:20 -0500

    Update Equipment.jsx

--- client/src/components/Tables/Equipment.jsx
+++ client/src/components/Tables/Equipment.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './Overview.css';
+
 
 function Equipment() {
   const [equipmentData, setEquipmentData] = useState([]);

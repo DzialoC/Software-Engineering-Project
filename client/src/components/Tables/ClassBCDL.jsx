8b343d364fc8380e81e705d635153d7357326991
TroyO4 <146477056+TroyO4@users.noreply.github.com>
2023-12-01 16:20:25 -0500

    Update ClassBCDL.jsx

--- client/src/components/Tables/ClassBCDL.jsx
+++ client/src/components/Tables/ClassBCDL.jsx
@@ -1,97 +1,45 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import './Overview.css';
 
-function ClassBCDL() {
-  const [classBCDLReports, setClassBCDLReports] = useState([]);
+function LatestReports() {
+  const [reports, setReports] = useState([]);
 
   useEffect(() => {
-    async function fetchClassBCDLReports() {
+    async function fetchReports() {
       try {
-        const response = await axios.get("http://localhost:5000/class-bcdl/", {
-          withCredentials: true,
-        });
-        setClassBCDLReports(response.data);
+        const response = await fetch("http://localhost:5000/latest-reports");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setReports(data);
       } catch (error) {
         console.error("Fetch error:", error);
       }
     }
 
-    fetchClassBCDLReports();
+    fetchReports();
   }, []);
 
   return (
     <div>
-      <h2>Class B CDL Reports</h2>
+      <h2>Latest Reports</h2>
       <table>
         <thead>
           <tr>
             <th>Report ID</th>
             <th>Vehicle ID</th>
             <th>User ID</th>
-            <th>Comment</th>
-            <th>Air/Hydraulic Brake Check</th>
-            <th>Parking Trailer Brake Check</th>
-            <th>Service Brake Check</th>
-            <th>Lighting Indicators</th>
-            <th>Emergency Equipment</th>
-            <th>Windshield Traffic Monitoring Devices</th>
-            <th>Wipers/Washers</th>
-            <th>Heater/Defroster</th>
-            <th>Horns</th>
-            <th>All External Lights</th>
-            <th>Lenses Front</th>
-            <th>Fluid Levels</th>
-            <th>Fluid Air Leaks</th>
-            <th>Steering Systems</th>
-            <th>Tires</th>
-            <th>Rims</th>
-            <th>Lug Nuts</th>
-            <th>Springs, Air Bags, Shocks</th>
-            <th>Brake Lines/Hoses/Leaks</th>
-            <th>Brake Contaminates</th>
-            <th>Lenses Reflectors (Side)</th>
-            <th>Traffic Monitoring Devices (Side)</th>
-            <th>Battery</th>
-            <th>Fuel Tanks</th>
-            <th>Frames</th>
-            <th>Lenses Reflectors (Rear)</th>
+            {/* Add other relevant headers */}
           </tr>
         </thead>
         <tbody>
-          {classBCDLReports.map((report) => (
+          {reports.map((report) => (
             <tr key={report.id}>
               <td>{report.id}</td>
               <td>{report.vehicleID}</td>
               <td>{report.userID}</td>
-              <td>{report.comment}</td>
-              <td>{report.airHydraulicBrakeCheck ? "Yes" : "No"}</td>
-              <td>{report.parkingTrailerBrakeCheck ? "Yes" : "No"}</td>
-              <td>{report.serviceBrakeCheck ? "Yes" : "No"}</td>
-              <td>{report.lightingIndicators ? "Yes" : "No"}</td>
-              <td>{report.emergencyEquipment ? "Yes" : "No"}</td>
-              <td>
-                {report.windshieldTrafficMonitoringDevices ? "Yes" : "No"}
-              </td>
-              <td>{report.wipersWashers ? "Yes" : "No"}</td>
-              <td>{report.heaterDefroster ? "Yes" : "No"}</td>
-              <td>{report.horns ? "Yes" : "No"}</td>
-              <td>{report.allExternalLights ? "Yes" : "No"}</td>
-              <td>{report.lensesFront ? "Yes" : "No"}</td>
-              <td>{report.fluidLevels ? "Yes" : "No"}</td>
-              <td>{report.fluidAirLeaks ? "Yes" : "No"}</td>
-              <td>{report.steeringSystems ? "Yes" : "No"}</td>
-              <td>{report.tires ? "Yes" : "No"}</td>
-              <td>{report.rims ? "Yes" : "No"}</td>
-              <td>{report.lugNuts ? "Yes" : "No"}</td>
-              <td>{report.springsAirBagsShocks ? "Yes" : "No"}</td>
-              <td>{report.brakeLinesHosesLeaks ? "Yes" : "No"}</td>
-              <td>{report.brakeContaminates ? "Yes" : "No"}</td>
-              <td>{report.lensesReflectorsSide ? "Yes" : "No"}</td>
-              <td>{report.trafficMonitoringDevicesSide ? "Yes" : "No"}</td>
-              <td>{report.battery ? "Yes" : "No"}</td>
-              <td>{report.fuelTanks ? "Yes" : "No"}</td>
-              <td>{report.frames ? "Yes" : "No"}</td>
-              <td>{report.lensesReflectorsRear ? "Yes" : "No"}</td>
+              {/* Display other report fields as needed */}
             </tr>
           ))}
         </tbody>
@@ -100,4 +48,4 @@ function ClassBCDL() {
   );
 }
 
-export default ClassBCDL;
+export default LatestReports;

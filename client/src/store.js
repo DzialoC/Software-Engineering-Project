f2adb864b4ec399637269c3925162854049d8a0c
Brandon Pacheco <BrandonAPacheco@Gmail.com>
2023-12-01 21:39:13 -0500

    Protected Route Function almost ready, Just need to address refresh bug

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,11 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/rootReducer.js"; // Import your root reducer
+import authReducer from "./reducers/authSlice.js";
+//import rootReducer from "./reducers/rootReducer.js"; // Import your root reducer
+
+const rootReducer = {
+  auth: authReducer,
+};
 
 const store = configureStore({
   reducer: rootReducer,
